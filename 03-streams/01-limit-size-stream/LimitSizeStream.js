08438b5e28caf0db860fa7d79cd8f30ffa1e5f54
Andrey Zabauka <azabauka@remedypointsolutions.com>
2021-06-04 17:47:09 +0300

    Tasks solution

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,21 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
-  constructor(options) {
-    super(options);
+  constructor({encoding, limit}) {
+    super();
+    this.limit = limit;
+    this.bufferSize = 0;
+    this.setEncoding(encoding);
   }
 
   _transform(chunk, encoding, callback) {
-
+    if (this.bufferSize + Buffer.byteLength(chunk) > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      this.bufferSize += Buffer.byteLength(chunk);
+      this.push(chunk);
+      callback();
+    }
   }
 }
 
--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,15 +1,45 @@
 const stream = require('stream');
-const os = require('os');
 
 class LineSplitStream extends stream.Transform {
-  constructor(options) {
-    super(options);
+  constructor({encoding}) {
+    super();
+    this.byteArray = [];
+    this.setEncoding(encoding);
   }
 
   _transform(chunk, encoding, callback) {
+    let flag = true;
+    for (let i = 0; i < chunk.length; i++) {
+      const byte = chunk[i];
+      switch (byte) {
+        case 0x0a:
+        case 0x0d:
+          if (flag) {
+            this.push(Buffer.concat(this.byteArray));
+            this.byteArray = [];
+            flag = false;
+          }
+          break;
+        default:
+          this.byteArray.push(chunk.slice(i, i+1));
+          flag = true;
+          break;
+      }
+    }
+    callback();
+  }
+
+  _final(callback) {
+    if (this.byteArray.length > 0 ) {
+      this.push(Buffer.concat(this.byteArray));
+      this.byteArray = [];
+    }
+    callback();
   }
 
   _flush(callback) {
+    this.byteArray = [];
+    callback();
   }
 }
 
